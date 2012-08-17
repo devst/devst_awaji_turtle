48d19f1f609a1bd1a4d1c3a10734401360225070
Kamiya Atsuteru <teru.k.business@gmail.com>
2012-08-18 00:04:13 +0900

    元に戻して、インデントだけ直した

--- src/main/java/features/turtle/FizzBuzz.java
+++ src/main/java/features/turtle/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz implements features.FizzBuzz {
 	@Override
 	public String fizzBuzz(int arg0) {
 		if(arg0 < 1){
-				throw new RuntimeException();
+			throw new RuntimeException();
 		}
 		if(arg0 % 15 == 0){
 			return "FizzBuzz";
@@ -13,9 +13,9 @@ public class FizzBuzz implements features.FizzBuzz {
 		if(arg0 % 3 == 0){
 			return "Fizz";
 		}
-//		if(arg0 % 5 == 0){
-//			return "Buzz";
-//		}
+		if(arg0 % 5 == 0){
+			return "Buzz";
+		}
 		return new Integer(arg0).toString();
 	}
 
